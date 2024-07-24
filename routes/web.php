b9134797f9c5bb268968d74dee3c719601241999
Derk <derkvanderheide@hotmail.com>
2024-07-24 15:59:42 +0200

    add routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/items', function () {
+})->name('items.index');
+Route::get('/items/create', function () {
+})->name('items.create');
+Route::post('/items', function () {
+})->name('items.store');
+Route::get('/items/{id}', function () {
+})->name('items.show');
+Route::get('/items/{id}/edit', function () {
+})->name('items.edit');
+Route::put('/items/{id}', function () {
+})->name('items.update');
+Route::delete('/items/{id}', function () {
+})->name('items.destroy');
